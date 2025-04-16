5445fb7fe8193a87cbfc3be95adf0733a0a575d1
Robson Paulo da Silva <robson_p_s@yahoo.com.br>
2025-04-16 00:21:14 -0300

    modified:   routes/index.js

--- routes/index.js
+++ routes/index.js
@@ -6,12 +6,10 @@ router.get("/", (req,res) => {res.send(`<html>
                 <title>Robson Paulo da Silva</title>
             </head>
             <body>
-                <h1>Welcome to Project: Contacts Part 2 Exercise</h1>
+                <h1>Welcome to the final projec</h1>
                 <p>Click on the links below</p>
                 <ul>
-                    <li><a href=https://cse-341-project1-t08e.onrender.com/contacts>All contacts</a>></li>
-                    <li><a href=https://cse-341-project1-t08e.onrender.com/contacts/67ca09d206dd9a333bc7be0e>Contact Id: ca09d206dd9a333bc7be0e </a></li>
-                </ul>
+                    <li><a href=https://cse-341-project1-t08e.onrender.com/products>All products</a>></li>
             </body>
         </html>`);});
 
